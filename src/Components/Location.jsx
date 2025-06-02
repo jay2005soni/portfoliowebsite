1e67313920c08ef7e4eea8da83a262edff70823c
Mukesh <mukeshpradhan54325@gmail.com>
2025-06-03 01:16:41 +0530

    Updates

--- src/Components/Location.jsx
+++
@@ -1,8 +0,0 @@
-function  Location(){
-    return(
-        <>
-        <div className="w-full p-2 rounded-5xl"><img src="src\assets\ourlocation.jpeg" alt="Our Location" /></div>
-        </>
-    )
-}
-export default Location;
